e1c95717af01878f94b60c1eb1c1a16db7d1d31b
Salar Ezaldeen <sa8ar_88@yahoo.com>
2020-08-24 00:33:34 +0300

    online action update

--- app/online_action.php
+++ app/online_action.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class online_action extends Model
 {
     //
+    public function customer_service()
+    {
+        return $this->belongsTo('App\customer_service');
+
+    }
+    public function online_account()
+    {
+       
+        
+            return $this->belongsToMany('App\online_account');
+        
+    }
+
+    public function takenby()
+    {
+        return $this->belongsTo('App\user','user_id');
+        
+    }
 }
